bc0fa663670d0e0327775fb20eddcfb7ca63a753
MrMagno <MrMagno@MrMagno>
2024-02-15 11:40:46 +0100

    Commit inicial

---
+++ src/entornos4/main.java
@@ -0,0 +1,36 @@
+package entornos4;
+
+/**
+ * @author Alejandro Lozano García
+ */
+public class main {
+
+public static void main(String[] args) {
+        eCesta cesta;
+        cesta = new eCesta("Apellidos del alumno",100, "dni del alumno", 10);
+
+        NuevoMetodo(cesta);        
+    }
+
+    public static void NuevoMetodo(eCesta cesta) {
+        try {
+            cesta.comprar(50);
+        } 
+        
+        catch (Exception e){
+            System.out.println("No se puede pagar  ");
+        } 
+        
+        try {
+            System.out.println("Solicitar actualización de bono");
+            cesta.ActualizarBono(100);
+        } 
+        
+        catch (Exception e) {
+            System.out.println("Error al recargar");
+        }
+
+        double bonoactual = cesta.bonificacion();
+        System.out.println("Su bono actual es  "+ bonoactual );        
+    }
+}
